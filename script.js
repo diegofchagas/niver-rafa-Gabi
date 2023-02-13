f55a42d9efa7bfa034a0aa32df3f7324feff2936
Diego Chagas <diego.chagas@afya.com.br>
2023-02-13 11:47:44 -0300

    subindo projeto de convite de aniversario

---
+++ script.js
@@ -0,0 +1,4 @@
+const rsvpButton = document.getElementById("rsvp");
+rsvpButton.addEventListener("click", function() {
+  window.location.href = "https://api.whatsapp.com/send?phone=5532991078240&text=Confirmo%20minha%20presen%C3%A7a!";
+});
